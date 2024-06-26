45b7c8c41b4877f6e93be3b62d6d871c5b2e2b30
Saro9033 <ssaravanan2310@gmail.com>
2024-06-26 21:02:33 +0530

    update some

--- src/API.jsx
+++ src/API.jsx
@@ -3,5 +3,5 @@ import axios from 'axios'
 axios.defaults.withCredentials = true;
 
 export default axios.create(
-    {baseURL:'https://dialforneed-server.vercel.app'}
+    {baseURL:'https://dialforneed-server.vercel.app/api'}
 )
